aaa0c8e671bd93a33d6e716571c23c8f80f11433
cjsimon <cjsimon333@gmail.com>
2024-10-08 20:04:25 -0400

    Fix shared workflow dependency

--- database/initdb/privileges.sql
+++ database/initdb/privileges.sql
@@ -1,3 +1,2 @@
 # TODO: Make compatible for non-dev envs; this is just for local development atm
-GRANT ALL PRIVILEGES ON *.* TO 'movie_manager_user'@'192.168.0.0/255.255.255.0' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'movie_manager_user'@'10.0.0.0/255.0.0.0' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'movie_manager_user'@'%' WITH GRANT OPTION;
